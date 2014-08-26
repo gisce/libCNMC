1f1523cd40e19c9cccf18233f0392aed488852a3
Eduard Carreras <ecarreras@gisce.net>
2014-08-26 15:03:22 +0200

    Número processadors cpus + 1

--- libcnmc/utils.py
+++ libcnmc/utils.py
@@ -2,7 +2,6 @@ import os
 import multiprocessing
 
 
-N_PROC = min(int(os.getenv('N_PROC', multiprocessing.cpu_count())),
-             multiprocessing.cpu_count())
+N_PROC = int(os.getenv('N_PROC', multiprocessing.cpu_count() + 1))
 
 
