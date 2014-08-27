fdcef6748107784133be69703592e34974542beb
Guillem Julià Blasi <gjulia@gisce.net>
2014-08-27 09:34:31 +0200

    Afegit carpeta de tests

---
+++ libcnmc/tests/test_f1_config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#Connection
+USER = 'admin'
+PWD = 'admin'
+URI = 'http://localhost'
+PORT = 8069
+DB = 'database'
+#F1
+OUTPUT = '/tmp/test_f1.csv'
+YEAR = '2013'
+CODI_R1 = '0000'
