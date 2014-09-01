e92a365ca92d3f2b9a463d21074f9085aa4c6bf5
Eduard Carreras <ecarreras@gmail.com>
2014-09-01 16:44:29 +0200

    Fix access only to ine

--- libcnmc/cir_4_2014/F11.py
+++ libcnmc/cir_4_2014/F11.py
@@ -34,7 +34,7 @@ class F11(MultiprocessBased):
     def get_ine(self, municipi_id):
         O = self.connection
         muni = O.ResMunicipi.read(municipi_id, ['ine', 'dc'])
-        return get_ine(O, muni['ine'])[1]
+        return get_ine(O, muni['ine'])
 
 
     def get_sortides_ct(self, ct_name):
