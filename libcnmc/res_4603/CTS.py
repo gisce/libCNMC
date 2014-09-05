30f818c4e0c443f31c5dd72692dfeef1a1411b9d
Pau Pericay <ppericay@gisce.net>
2014-09-05 16:01:42 +0200

    New script Centres Transformadors

---
+++ libcnmc/res_4603/CTS.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+INVENTARI DE CNMC Centres Transformadors
+"""
+from datetime import datetime
+import traceback
+
+from libcnmc.core import MultiprocessBased
+
+
+class CTS(MultiprocessBased):
+    def __init__(self, **kwargs):
+        super(CTS, self).__init__(**kwargs)
+        self.year = kwargs.pop('year', datetime.now().year - 1)
+        self.codi_r1 = kwargs.pop('codi_r1')
+        self.base_object = 'Línies CTS'
+        self.report_name = 'CNMC INVENTARI CTS'
+
+    def get_sequence(self):
+        search_params = [('name', '!=', '1')]
+        return self.connection.GiscedataCts.search(search_params)
+
+    def consumer(self):
+        O = self.connection
+        while True:
+            try:
+                item = self.input_q.get()
+                self.progress_q.put(item)
+
+                ct = O.GiscedataCts.get(item)
+                codi = ct.codi_instalacio
+                try:
+                    data_industria = ct.expedients_ids[0].industria_data
+                    if data_industria:
+                        data_industria = datetime.strptime(str(data_industria),
+                                                           '%Y-%m-%d')
+                        data_aps = data_industria.strftime('%d/%m/%Y')
+                    else:
+                        data_pm = datetime.strptime(str(ct.data_pm), '%Y-%m-%d')
+                        data_aps = data_pm.strftime('%d/%m/%Y')
+                except Exception as e:
+                    print "error: %d %s" % (item, e)
+                    data_aps = ''
+
+                ccaa = ct.id_municipi.state.comunitat_autonoma.codi
+                finan = ct.perc_financament
+
+                output = [
+                    '%s' % ct.name,
+                    ct.cini or '',
+                    ct.descripcio or '',
+                    codi or '',
+                    ccaa or '',
+                    round(100 - int(finan)),
+                    data_aps,
+                    ''
+                ]
+
+                self.output_q.put(output)
+            except:
+                traceback.print_exc()
+                if self.raven:
+                    self.raven.captureException()
+            finally:
+                self.input_q.task_done()
