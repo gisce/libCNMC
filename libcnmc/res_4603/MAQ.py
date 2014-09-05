2cec0e7edc0efc8f7b9d4d13e8f3bb65015c8b98
Pau Pericay <ppericay@gisce.net>
2014-09-05 15:16:41 +0200

    Afegit l'script de les Màquines.

---
+++ libcnmc/res_4603/MAQ.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+INVENTARI DE CNMC Maquines
+"""
+from datetime import datetime
+import traceback
+
+from libcnmc.core import MultiprocessBased
+
+
+class MAQ(MultiprocessBased):
+    def __init__(self, **kwargs):
+        super(MAQ, self).__init__(**kwargs)
+        self.year = kwargs.pop('year', datetime.now().year - 1)
+        self.codi_r1 = kwargs.pop('codi_r1')
+        self.base_object = 'Línies MAQ'
+        self.report_name = 'CNMC INVENTARI MAQ'
+
+    def get_sequence(self):
+        search_params = [('name', '!=', '1'),
+                         ('id_estat.cnmc_inventari', '=', True)]
+        return self.connection.GiscedataTransformadorTrafo.search(search_params)
+
+    def consumer(self):
+        O = self.connection
+        while True:
+            try:
+                item = self.input_q.get()
+                self.progress_q.put(item)
+
+                trafo = O.GiscedataTransformadorTrafo.read(
+                    item, ['cini', 'historic', 'data_aps', 'ct', 'name',
+                           'potencia_nominal', 'codi_instalacio'])
+
+                #La propia empresa
+                company = O.ResCompany.get(1)
+                if trafo['codi_instalacio']:
+                    codi = trafo['codi_instalacio']
+
+                if trafo['historic']:
+                    historic = O.GiscedataTransformadorHistoric.read(
+                        trafo['historic'][0], ['data_entrada'])
+                    data_hist = historic['data_entrada']
+                for hist_id in trafo['historic']:
+                    historic = O.GiscedataTransformadorHistoric.read(
+                        hist_id, ['data_entrada'])
+                    if historic['data_entrada'] < data_hist:
+                        data_hist = historic['data_entrada']
+
+                data_pm = trafo['data_aps'] or data_hist
+                if data_pm:
+                    data_pm = datetime.strptime(str(data_pm),
+                                                '%Y-%m-%d %H:%M:%S')
+                    data_pm = data_pm.strftime('%d/%m/%Y')
+
+                comunitat = ''
+                c_ccaa = ''
+                finan = 0
+                if trafo['ct']:
+                    cts = O.GiscedataCts.read(trafo['ct'][0],
+                                              ['id_municipi',
+                                               'perc_financament'])
+                    id_comunitat = O.ResComunitat_autonoma.\
+                        get_ccaa_from_municipi([], cts['id_municipi'][0])
+                    comunidad = O.ResComunitat_autonoma.read(id_comunitat,
+                                                             ['codi'])
+                    if comunidad:
+                        comunitat = comunidad[0]['codi']
+                    finan = cts['perc_financament']
+                if len(company.partner_id.address):
+                    if company.partner_id.address[0].state_id:
+                        c_ccaa = company.partner_id.address[0].\
+                            state_id.comunitat_autonoma.codi
+
+                output = [
+                    '%s' % trafo['name'],
+                    trafo['cini'] or '',
+                    trafo['name'] or '',
+                    codi or '',
+                    '',
+                    comunitat or c_ccaa or '',
+                    round(100 - int(finan)),
+                    data_pm,
+                    '',
+                    trafo['potencia_nominal']
+                ]
+
+                self.output_q.put(output)
+            except:
+                traceback.print_exc()
+                if self.raven:
+                    self.raven.captureException()
+            finally:
+                self.input_q.task_done()
