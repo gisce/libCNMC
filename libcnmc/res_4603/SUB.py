8de6e9edb88cc682cd9f07dceabe8c0c3d6b2141
Agustí Fita i Borrell <afita@gisce.net>
2015-04-01 18:03:46 +0200

    IMP Dades administratives per Màquines (trafos)
    * Tenim en compte propietari i data_pm al seleccionar els CT's
    * Millorem la cerca de municipi/ccaa
    * La lògica de expedients i dates de posta en marxa la té el propi model
      (data_pm)

--- libcnmc/res_4603/SUB.py
+++ libcnmc/res_4603/SUB.py
@@ -23,6 +23,10 @@ class SUB(MultiprocessBased):
 
     def get_sequence(self):
         search_params = []
+        data_pm = '%s-01-01' % (self.year + 1)
+        search_params += [('propietari', '=', True),
+                          '|', ('data_pm', '=', False),
+                               ('data_pm', '<', data_pm)]
         return self.connection.GiscedataCtsSubestacions.search(search_params)
 
     def consumer(self):
@@ -37,42 +41,45 @@ class SUB(MultiprocessBased):
                 sub = O.GiscedataCtsSubestacions.read(item, fields_to_read)
 
                 if not sub:
+                    txt = ("**** ERROR: El ct %s (id:%s) no està en "
+                           "giscedata_cts_subestacions.\n" %
+                           (sub['name'], sub['id']))
+
                     if not QUIET:
-                        sys.stderr.write("**** ERROR: El ct %s (id:%s) no està "
-                                         "en giscedata_cts_subestacions.\n"
-                                         % (sub['name'], sub['id']))
+                        sys.stderr.write(txt)
                         sys.stderr.flush()
 
+                    raise Exception(txt)
+
                 # Calculem any posada en marxa
-                data_pm = sub['data_industria'] or sub['data_pm']
+                data_pm = sub['data_pm']
 
                 if data_pm:
                     data_pm = datetime.strptime(str(data_pm), '%Y-%m-%d')
                     data_pm = data_pm.strftime('%d/%m/%Y')
 
                 comunitat = ''
+                id_municipi = None
                 if sub['id_municipi']:
-                    municipi = O.ResMunicipi.read(sub['id_municipi'][0],
-                                                  ['state'])
-                    if municipi['state']:
-                        provincia = O.ResCountryState.read(
-                            municipi['state'][0],
-                            ['comunitat_autonoma'])
-                        if provincia['comunitat_autonoma']:
-                            comunitat = provincia['comunitat_autonoma'][0]
+                    id_municipi = sub['id_municipi'][0]
                 else:
                     #Si no hi ha subestació agafem la comunitat del rescompany
                     company_partner = O.ResCompany.read(1, ['partner_id'])
                     #funció per trobar la ccaa desde el municipi
-                    fun_ccaa = O.ResComunitat_autonoma.get_ccaa_from_municipi
                     if company_partner:
                         address = O.ResPartnerAddress.read(
                             company_partner['partner_id'][0], ['id_municipi'])
                         if address['id_municipi']:
-                            id_comunitat = fun_ccaa(address['id_municipi'][0])
-                            comunidad = O.ResComunitat_autonoma.read(
-                                id_comunitat, ['codi'])
-                            comunitat = comunidad[0]['codi']
+                            id_municipi = address['id_municipi'][0]
+
+                if id_municipi:
+                    #funció per trobar la ccaa desde el municipi
+                    fun_ccaa = O.ResComunitat_autonoma.get_ccaa_from_municipi
+                    id_comunitat = fun_ccaa(id_municipi)
+                    comunitat_vals = O.ResComunitat_autonoma.read(
+                        id_comunitat[0], ['codi'])
+                    if comunitat_vals:
+                        comunitat = comunitat_vals['codi']
 
                 output = [
                     '%s' % sub['name'],
