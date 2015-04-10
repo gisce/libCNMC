12346ad3c541d6b0c63226058383b691d5529225
Guillem Julià Blasi <gjulia@gisce.net>
2015-04-10 13:18:41 +0200

    Afegir validació data_baixa i actius a Subest.

--- libcnmc/res_4603/SUB.py
+++ libcnmc/res_4603/SUB.py
@@ -24,10 +24,20 @@ class SUB(MultiprocessBased):
     def get_sequence(self):
         search_params = []
         data_pm = '%s-01-01' % (self.year + 1)
+        data_baixa = '%s-12-31' % self.year
         search_params += [('propietari', '=', True),
                           '|', ('data_pm', '=', False),
-                               ('data_pm', '<', data_pm)]
-        return self.connection.GiscedataCtsSubestacions.search(search_params)
+                               ('data_pm', '<', data_pm),
+                          '|', ('data_baixa', '>', data_baixa),
+                               ('data_baixa', '=', False)
+                          ]
+        # Revisem que si està de baixa ha de tenir la data informada.
+        search_params += ['|',
+                          '&', ('active', '=', False),
+                               ('data_baixa', '!=', False),
+                          ('active', '=', True)]
+        return self.connection.GiscedataCtsSubestacions.search(
+            search_params, 0, 0, False, {'active_test': False})
 
     def consumer(self):
         O = self.connection
