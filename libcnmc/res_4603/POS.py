cc9d92d1f327b481d7fcb7f1b8ff405d482c5991
Guillem Julià Blasi <gjulia@gisce.net>
2015-04-22 18:51:13 +0200

    Using function get_id_municpi_from_company POS

--- libcnmc/res_4603/POS.py
+++ libcnmc/res_4603/POS.py
@@ -9,6 +9,7 @@ import traceback
 import sys
 
 from libcnmc.core import MultiprocessBased
+from libcnmc.utils import get_id_municipi_from_company
 
 QUIET = False
 
@@ -77,14 +78,7 @@ class POS(MultiprocessBased):
                 if cts['id_municipi']:
                     id_municipi = cts['id_municipi'][0]
                 else:
-                    #Si no hi ha ct agafem la comunitat del rescompany
-                    company_partner = O.ResCompany.read(1, ['partner_id'])
-                    #funció per trobar la ccaa desde el municipi
-                    if company_partner:
-                        address = O.ResPartnerAddress.read(
-                            company_partner['partner_id'][0], ['id_municipi'])
-                        if address['id_municipi']:
-                            id_municipi = address['id_municipi'][0]
+                    id_municipi = get_id_municipi_from_company(O)
 
                 if id_municipi:
                     #funció per trobar la ccaa desde el municipi
