84ed147c71583014599f1554fbae81120288bb8f
Axel Simón <asimon@gisce.net>
2015-08-17 14:11:24 +0200

    F13c form added.

--- libcnmc/cir_4_2015/F13C.py
+++ libcnmc/cir_4_2015/F13C.py
@@ -12,23 +12,56 @@ from libcnmc.core import MultiprocessBased
 class F13c(MultiprocessBased):
     def __init__(self, **kwargs):
         super(F13c, self).__init__(**kwargs)
+        self.year = kwargs.pop('year', datetime.now().year - 1)
+        self.report_name = 'F13 C - CTS'
+        self.base_object = 'CTS'
 
     def get_sequence(self):
-        search_params = []
-        return self.connection.GiscedataCupsPs.search(search_params)
+        search_params = [('interruptor', '=', '2')]
+        data_pm = '%s-01-01' % (self.year + 1)
+        data_baixa = '%s-12-31' % self.year
+        search_params += [('propietari', '=', True),
+                          '|', ('data_pm', '=', False),
+                               ('data_pm', '<', data_pm),
+                          '|', ('data_baixa', '>', data_baixa),
+                               ('data_baixa', '=', False),
+                          ]
+        # Revisem que si està de baixa ha de tenir la data informada.
+        search_params += ['|',
+                          '&', ('active', '=', False),
+                               ('data_baixa', '!=', False),
+                          ('active', '=', True)]
+        return self.connection.GiscedataCtsSubestacionsPosicio.search(
+            search_params, 0, 0, False, {'active_test': False})
+
+    def get_tensio(self, sub):
+        o = self.connection
+        res = ''
+        if sub['tensio']:
+            tensio_obj = o.GiscedataTensionsTensio.read(sub['tensio'][0])
+            res = tensio_obj['tensio']
+        return res
 
     def consumer(self):
         o = self.connection
+        fields_to_read = [
+            'name', 'cini', 'propietari', 'tipus_posicio', 'data_pm', 'tensio'
+        ]
         while True:
             try:
                 # generar linies
-                o_subestacio = ''
-                o_parc = ''
-                o_pos = ''
-                o_cini = ''
-                o_prop = ''
-                o_data = ''
-                o_any = ''
+                item = self.input_q.get()
+                self.progress_q.put(item)
+                sub = o.GiscedataCtsSubestacionsPosicio.read(
+                    item, fields_to_read
+                )
+                o_subestacio = sub['name']
+                o_parc = self.get_tensio(sub)
+                o_pos = sub['tipus_posicio']
+                o_cini = sub['cini']
+                o_prop = int(sub['propietari'])
+                o_data = sub['data_pm']
+                o_any = self.year + 1
 
                 self.output_q.put([
                     o_subestacio,
