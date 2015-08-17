0a423665d99898e35e62734d48d507a8d1d99b6c
Axel Simón <asimon@gisce.net>
2015-08-17 13:54:26 +0200

    F14 and F15 forms added.

--- libcnmc/cir_4_2015/F14.py
+++ libcnmc/cir_4_2015/F14.py
@@ -5,42 +5,88 @@ import re
 import traceback
 
 from libcnmc.utils import CODIS_TARIFA, CODIS_ZONA, CINI_TG_REGEXP
-from libcnmc.utils import get_ine, get_comptador
+from libcnmc.utils import get_ine, get_comptador, get_norm_tension
 from libcnmc.core import MultiprocessBased
 
 
 class F14(MultiprocessBased):
     def __init__(self, **kwargs):
         super(F14, self).__init__(**kwargs)
+        self.year = kwargs.pop('year', datetime.now().year - 1)
+        self.report_name = 'F14 - CTS'
+        self.base_object = 'CTS'
 
     def get_sequence(self):
-        search_params = []
-        return self.connection.GiscedataCupsPs.search(search_params)
+        search_params = [
+            ('reductor', '=', True),
+            ('id_estat.cnmc_inventari', '=', True)
+        ]
+        return self.connection.GiscedataTransformadorTrafo.search(
+            search_params
+        )
+
+    def get_estat(self, estat_id):
+        o = self.connection
+        estat = o.GiscedataTransformadorEstat.read(estat_id, ['codi'])
+        if estat['codi'] != 1:
+            return 0
+        else:
+            return 1
+
+    def get_costat_alta(self, trafo):
+        o = self.connection
+        res = ''
+        if trafo['conexions']:
+            con = o.GiscedataTransformadorConnexio.read(trafo['conexions'][0])
+            tensio = con['tensio_primari']
+            res = get_norm_tension(o, tensio)
+        return res
+
+    def get_costat_baixa(self, trafo):
+        o = self.connection
+        res = ''
+        if trafo['conexions']:
+            con = o.GiscedataTransformadorConnexio.read(trafo['conexions'][0])
+            tensio = con['tensio_b1']
+            res = get_norm_tension(o, tensio)
+        return res
+
 
     def consumer(self):
         o = self.connection
+        fields_to_read = [
+            'ct', 'name', 'cini', 'potencia_nominal', 'propietari', 'id_estat',
+            'conexions'
+        ]
         while True:
             try:
                 # generar linies
-                o_subestacio = ''
-                o_maquina = ''
-                o_alta = ''
-                o_baixa = ''
-                o_pot_maquina = ''
+                item = self.input_q.get()
+                self.progress_q.put(item)
+                trafo = o.GiscedataTransformadorTrafo.read(
+                    item, fields_to_read
+                )
+                o_subestacio = trafo['ct'][1]
+                o_maquina = trafo['name']
+                o_cini = trafo['cini']
+                o_costat_alta = self.get_costat_alta(trafo)
+                o_costat_baixa = self.get_costat_baixa(trafo)
+                o_pot_maquina = trafo['potencia_nominal'] / 1000.0
                 o_pot_activa = ''
                 o_pot_reactiva = ''
                 o_energia_anual = ''
                 o_perdues = ''
                 o_perdues_nominal = ''
-                o_propietat = ''
-                o_estat = ''
-                o_any = ''
+                o_propietat = int(trafo['propietari'])
+                o_estat = self.get_estat(trafo['id_estat'][0])
+                o_any = self.year + 1
 
                 self.output_q.put([
                     o_subestacio,
                     o_maquina,
-                    o_alta,
-                    o_baixa,
+                    o_cini,
+                    o_costat_alta,
+                    o_costat_baixa,
                     o_pot_maquina,
                     o_pot_activa,
                     o_pot_reactiva,
--- libcnmc/cir_4_2015/F15.py
+++ libcnmc/cir_4_2015/F15.py
@@ -12,34 +12,97 @@ from libcnmc.core import MultiprocessBased
 class F15(MultiprocessBased):
     def __init__(self, **kwargs):
         super(F15, self).__init__(**kwargs)
+        self.year = kwargs.pop('year', datetime.now().year - 1)
+        self.codi_r1 = kwargs.pop('codi_r1')
+        self.report_name = 'F15 - CTS'
+        self.base_object = 'CTS'
 
     def get_sequence(self):
-        search_params = []
-        return self.connection.GiscedataCupsPs.search(search_params)
+        search_params = [
+            # ('inventari', '=', 'fiabilitat'),
+            ('installacio', 'ilike', 'giscedata.at.suport,%')
+        ]
+        return self.connection.GiscedataCellesCella.search(search_params)
+
+    def get_node_vertex(self, installacio):
+        o = self.connection
+        valor = str.split(installacio, ',')
+        id_tram = int(valor[1])
+        node = ''
+        vertex = ('', '')
+        if id_tram:
+            bloc = o.GiscegisBlocsCtat.read(id_tram, ['node', 'vertex'])
+            if bloc:
+                node = bloc['node'][0]
+                if bloc['vertex']:
+                    v = o.GiscegisVertex.read(bloc['vertex'][0], ['x', 'y'])
+                    vertex = (round(v['x'], 3), round(v['y'], 3))
+        return node, vertex
+
+    def obtenir_tram(self, installacio):
+        o = self.connection
+        valor = str.split(installacio, ',')
+        id_tram = int(valor[1])
+        tram = o.GiscedataAtSuport.read(id_tram, ['name'])
+        valor = tram['name']
+        return valor
+
+    def obtenir_camps_linia(self, installacio):
+        o = self.connection
+        valor = str.split(installacio, ',')
+        id_tram = int(valor[1])
+        tram = o.GiscedataAtSuport.read(id_tram, ['linia'])
+        linia_id = tram['linia']
+        fields_to_read = [
+            'municipi', 'provincia', 'tensio'
+        ]
+        linia = o.GiscedataAtLinia.read(int(linia_id[0]), fields_to_read)
+        municipi = linia['municipi'][0]
+        provincia = linia['provincia'][0]
+        tensio = linia['tensio']
+        res = {
+            'municipi': municipi,
+            'provincia': provincia,
+            'tensio': tensio
+        }
+        return res
 
     def consumer(self):
         o = self.connection
+        fields_to_read = [
+            'inventari', 'installacio', 'cini', 'propietari'
+        ]
         while True:
             try:
                 # generar linies
-                o_node = ''
-                o_fiabilitat = ''
-                o_tram = ''
-                o_cini = ''
-                o_utm = ''
-                o_municipi = ''
-                o_provincia = ''
-                o_tensio = ''
-                o_cod_dis = ''
-                o_prop = ''
-                o_any = ''
+                item = self.input_q.get()
+                self.progress_q.put(item)
+                celles = o.GiscedataCellesCella.read(
+                    item, fields_to_read
+                )
+                dict_linia = self.obtenir_camps_linia(celles['installacio'])
+                o_node, vertex = self.get_node_vertex(celles['installacio'])
+                o_fiabilitat = celles['inventari']
+                o_tram = self.obtenir_tram(celles['installacio'])
+                o_cini = celles['cini']
+                o_utm_x = vertex[0]
+                o_utm_y = vertex[1]
+                o_utm_z = ''
+                o_municipi = dict_linia.get('municipi')
+                o_provincia = dict_linia.get('provincia')
+                o_tensio = dict_linia.get('tensio')
+                o_cod_dis = self.codi_r1
+                o_prop = int(celles['propietari'])
+                o_any = self.year + 1
 
                 self.output_q.put([
                     o_node,
                     o_fiabilitat,
                     o_tram,
                     o_cini,
-                    o_utm,
+                    o_utm_x,
+                    o_utm_y,
+                    o_utm_z,
                     o_municipi,
                     o_provincia,
                     o_tensio,
