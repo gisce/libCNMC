5e6718c8e6340f9b46eba92b227a9c13496601da
Axel Simón <asimon@gisce.net>
2015-08-19 14:57:58 +0200

    Owner filter removed.

--- libcnmc/cir_4_2015/F12bis.py
+++ libcnmc/cir_4_2015/F12bis.py
@@ -23,9 +23,8 @@ class F12bis(MultiprocessBased):
         ]
         data_pm = '%s-01-01' % (self.year + 1)
         data_baixa = '%s-12-31' % self.year
-        search_params += [('propietari', '=', True),
-                          '|', ('data_pm', '=', False),
-                               ('data_pm', '<', data_pm),
+        search_params += [('data_pm', '=', False),
+                          ('data_pm', '<', data_pm),
                           '|', ('data_baixa', '>', data_baixa),
                                ('data_baixa', '=', False)
                           ]
