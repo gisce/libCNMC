bb2aad109521b492ae8f1451daa4ecccd6146c7f
Axel Simón <asimon@gisce.net>
2015-08-19 08:15:58 +0200

    Remaining F12 form fields added.

--- libcnmc/cir_4_2015/F12.py
+++ libcnmc/cir_4_2015/F12.py
@@ -12,35 +12,60 @@ from libcnmc.core import MultiprocessBased
 class F12(MultiprocessBased):
     def __init__(self, **kwargs):
         super(F12, self).__init__(**kwargs)
+        self.year = kwargs.pop('year', datetime.now().year - 1)
+        self.report_name = 'F12 - CTS'
+        self.base_object = 'CTS'
 
     def get_sequence(self):
         search_params = []
-        return self.connection.GiscedataCupsPs.search(search_params)
+        return self.connection.GiscedataTransformadorTrafo.search(
+            search_params
+        )
+
+    def get_node(self, ct_id):
+        o = self.connection
+        bloc = o.GiscegisBlocsCtat.search([('ct', '=', ct_id)])
+        node = ''
+        if bloc:
+            bloc = o.GiscegisBlocsCtat.read(bloc[0], ['node'])
+            node = bloc['node'][0]
+        return node
 
     def consumer(self):
         o = self.connection
+        fields_to_read = [
+            'ct', 'name', 'cini', 'potencia_nominal', 'propietari',
+            'perdues_buit', 'perdues_curtcircuit_nominal'
+        ]
         while True:
             try:
                 # generar linies
-                o_node = ''
-                o_ct = ''
-                o_cini = ''
-                o_maquina = ''
-                o_pot_max = ''
-                o_perdues_buit = ''
-                o_perdues_nominal = ''
-                o_propietat = ''
-                o_any = ''
+                item = self.input_q.get()
+                self.progress_q.put(item)
+                trafo = o.GiscedataTransformadorTrafo.read(
+                    item, fields_to_read
+                )
+                o_node = self.get_node(item)
+                o_ct = trafo['ct'] or ''
+                if o_ct != '':
+                    o_ct = str(o_ct[1])
+                o_cini = trafo['cini'] or ''
+                o_maquina = trafo['name']
+                o_pot = trafo['potencia_nominal'] / 1000.0
+                o_perdues_buit = trafo['perdues_buit'] or 0
+                o_perdues_nominal = trafo['perdues_curtcircuit_nominal'] or 0
+                o_propietari = int(trafo['propietari'])
+                o_any = self.year + 1
 
                 self.output_q.put([
                     o_node,
                     o_ct,
                     o_cini,
                     o_maquina,
-                    o_pot_max,
+                    o_pot,
                     o_perdues_buit,
                     o_perdues_nominal,
-                    o_propietat,
+                    o_propietari,
                     o_any
                 ])
             except:
