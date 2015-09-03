f8d7d1a88132ab5f746c26729c9f82b7bdb26613
Axel Simón <asimon@gisce.net>
2015-09-03 16:31:32 +0200

    F9 form data now matches with F10.
    Code has been optimised.

--- libcnmc/cir_4_2015/F9.py
+++ libcnmc/cir_4_2015/F9.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import traceback
 from libcnmc.core import MultiprocessBased
+from libcnmc.utils import get_srid, convert_srid, format_f
 try:
     from cStringIO import StringIO
 except:
@@ -13,48 +14,19 @@ class F9(MultiprocessBased):
         super(F9, self).__init__(**kwargs)
         self.year = kwargs.pop('year', datetime.now().year - 1)
         self.report_name = 'F9 - CTS'
+        self.codi_r1 = ''
         self.base_object = 'CTS'
+        o = self.connection
+        wiz_obj = o.WizardCircular4_2015
+        self.codi_r1 = wiz_obj.default_get(['codi_r1'])['codi_r1']
+        id_res_like = o.ResConfig.search(
+            [('name', '=', 'giscegis_btlike_layer')])
+        self.layer = '%\_BT\_%'
+        if id_res_like:
+            self.layer = o.ResConfig.read(id_res_like, ['value'])[0]['value']
 
     def get_sequence(self):
-        search_params = []
-        return self.connection.GiscedataCupsPs.search(search_params)
-
-    def get_geom(self, id_tram, net):
         o = self.connection
-        model_edge = o.GiscegisEdge
-        model_vertex = o.GiscegisPolylineVertex
-        model_polyline = o.GiscegisPolyline
-        data = []
-        if net.lower() == 'at':
-
-            ids_edges = model_edge.search(
-                [('id_linktemplate', '=', id_tram),
-                 ('layer', 'not ilike', '%\_BT\_%')]
-            )
-        else:
-            ids_edges = model_edge.search(
-                [('id_linktemplate', '=', id_tram),
-                 ('layer', 'not ilike', '%\_AT\_%')]
-            )
-        edges = model_edge.read(ids_edges)
-        if not edges:
-            return []
-        vertexs = model_polyline.read(edges[0]['polyline'][0])
-        for punt in model_vertex.read(vertexs['vertex_ids']):
-            data.append({'x': punt['x'], 'y': punt['y']})
-        return data
-
-    def conv_text(self, data):
-        t = ''
-        for line in data:
-            t += '{0};{1};{2}\n'.format(line['x'], line['y'], 0)
-        return t[:-1]
-
-    def consumer(self):
-        o = self.connection
-        fields_to_read = [
-            'name'
-        ]
         data_pm_limit = '%s-01-01' % (self.year + 1)
         data_baixa = '%s-12-31' % self.year
         static_search_params = ['|', ('data_pm', '=', False),
@@ -67,30 +39,100 @@ class F9(MultiprocessBased):
                                  '&', ('active', '=', False),
                                  ('data_baixa', '!=', False),
                                  ('active', '=', True)]
+        # AT
+        ids_at = 0
+        trams = []
+        ids_bt = 0
+        ids_linia_at = o.GiscedataAtLinia.search([])
+        linia = o.GiscedataAtLinia.read(ids_linia_at, ['trams'])
+        for elem in linia:
+            trams += elem['trams']
+        search_params = [('id', 'in', trams)]
+        search_params += static_search_params
+        ids_at = o.GiscedataAtTram.search(
+            search_params, 0, 0, False, {'active_test': False})
+
+        # BT
+
+        search_params = [
+            ('cable.tipus.codi', 'in', ['T', 'D', 'S'])
+        ]
+        search_params += static_search_params
+        ids_bt = o.GiscedataBtElement.search(
+            search_params, 0, 0, False, {'active_test': False})
+
+        # IDS AT + BT
+
+        ids = []
+
+        for at in ids_at:
+            ids.append((at, 'at'))
+        for bt in ids_bt:
+            ids.append((bt, 'bt'))
+
+        # print "ids AT: {0}".format(len(ids_at))
+        # print "ids BT: {0}".format(len(ids_bt))
+
+        return ids
+
+    def get_geom(self, id_tram, net):
+        o = self.connection
+        like_layer = self.layer
+        model_edge = o.GiscegisEdge
+        model_vertex = o.GiscegisPolylineVertex
+        model_polyline = o.GiscegisPolyline
+        data = []
+        if net.lower() == 'at':
+            ids_edges = model_edge.search(
+                [('id_linktemplate', '=', id_tram),
+                 ('layer', 'not ilike', like_layer)]
+            )
+            # print "ids edges at: {0}".format(len(ids_edges))
+        else:
+            ids_edges = model_edge.search(
+                [('id_linktemplate', '=', id_tram),
+                 ('layer', 'ilike', like_layer)]
+            )
+            # print "ids edges bt: {0}".format(len(ids_edges))
+        edges = model_edge.read(ids_edges)
+        if not edges:
+            return []
+        vertexs = model_polyline.read(edges[0]['polyline'][0])
+        for punt in model_vertex.read(vertexs['vertex_ids']):
+            data.append({'x': format_f(punt['x'], decimals=6),
+                         'y': format_f(punt['y'], decimals=6)})
+        return data
+
+    def conv_text(self, data):
+        o = self.connection
+        t = ''
+        for line in data:
+            res_srid = convert_srid(self.codi_r1, get_srid(o),
+                                    [line['x'], line['y']])
+            t += '{0};{1};{2}\n'.format(res_srid[0], res_srid[1], 0)
+        return t[:-1]
+
+    def consumer(self):
+        o = self.connection
         while True:
             try:
                 # generar linies
                 item = self.input_q.get()
                 self.progress_q.put(item)
-                linia = o.GiscedataAtLinia.read(item, ['trams'])
-                search_params = [('id', 'in', linia['trams'])]
-                search_params += static_search_params
-                ids = o.GiscedataAtTram.search(
-                    search_params, 0, 0, False, {'active_test': False})
-                for at in o.GiscedataAtTram.read(ids, fields_to_read):
+                if item[1] == 'at':
+                    at = o.GiscedataAtTram.read(item[0], ['name'])
                     data = self.get_geom(at['name'], 'at')
                     data = self.conv_text(data)
-                    if data:
-                        self.output_q.put(['A' + str(at['name'])])
-                        self.output_q.put([data])
-                        self.output_q.put(['END'])
-                for bt in o.GiscedataBtElement.read(ids, fields_to_read):
-                    data = self.get_geom(bt['name'], 'at')
+                    self.output_q.put(['A' + str(at['name'])])
+                    self.output_q.put([data])
+                    self.output_q.put(['END'])
+                else:
+                    bt = o.GiscedataBtElement.read(item[0], ['name'])
+                    data = self.get_geom(bt['name'], 'bt')
                     data = self.conv_text(data)
-                    if data:
-                        self.output_q.put(['B' + str(bt['name'])])
-                        self.output_q.put([data])
-                        self.output_q.put(['END'])
+                    self.output_q.put(['B' + str(bt['name'])])
+                    self.output_q.put([data])
+                    self.output_q.put(['END'])
             except:
                 traceback.print_exc()
                 if self.raven:
