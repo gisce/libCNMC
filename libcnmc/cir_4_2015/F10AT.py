79af5c6fd79321fefc0be99c1505400c54ccdfb3
Guillem Julià Blasi <gjulia@gisce.net>
2015-09-08 12:34:15 +0200

    Fix duplicate longitud_cad

--- libcnmc/cir_4_2015/F10AT.py
+++ libcnmc/cir_4_2015/F10AT.py
@@ -34,8 +34,7 @@ class F10AT(MultiprocessBased):
         o = self.connection
         fields_to_read = [
             'name', 'cini', 'circuits', 'longitud_cad', 'linia', 'origen',
-            'final', 'coeficient', 'cable', 'tensio_max_disseny',
-            'longitud_cad'
+            'final', 'coeficient', 'cable', 'tensio_max_disseny'
         ]
         data_pm_limit = '%s-01-01' % (self.year + 1)
         data_baixa = '%s-12-31' % self.year
