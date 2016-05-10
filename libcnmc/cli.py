0f171c29e1cadedc3c086ada02e92a8b48b59530
Xavier Barnada <xbarnada@gisce.net>
2016-05-10 12:38:30 +0200

    Improve cli with LAT 4131 and allowing to run cli.py

--- libcnmc/cli.py
+++ libcnmc/cli.py
@@ -1453,3 +1453,32 @@ def cir_4_2015_f16(**kwargs):
 
 def invoke():
     cnmc()
+
+
+@cnmc.command()
+@click.option('-q', '--quiet', default=False,
+              help="No mostrar missatges de status per stderr")
+@click.option('--interactive/--no-interactive', default=True,
+              help="Deshabilitar el mode interactiu")
+@click.option('-o', '--output', help="Fitxer de sortida")
+@click.option('-c', '--codi-r1', help='Codi R1 de la distribuidora')
+@click.option('-y', '--year', default=(datetime.now().year - 1),
+              help=u"Any per càlculs")
+@click.option('-s', '--server', default='http://localhost',
+              help=u'Adreça servidor ERP')
+@click.option('-p', '--port', default=8069, help='Port servidor ERP',
+              type=click.INT)
+@click.option('-u', '--user', default='admin', help='Usuari servidor ERP')
+@click.option('-w', '--password', default='admin',
+              help='Contrasenya usuari ERP')
+@click.option('-d', '--database', help='Nom de la base de dades')
+@click.option('--embarrats/--no-embarrats', default=False,
+              help="Afegir embarrats")
+@click.option('--num-proc', default=N_PROC, type=click.INT)
+def res_4131_lat(**kwargs):
+    from libcnmc.res_4131 import LAT
+    res_lat(LAT, **kwargs)
+
+
+if __name__ == '__main__':
+    invoke()
