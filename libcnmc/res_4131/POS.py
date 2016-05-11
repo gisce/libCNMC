1652407435471de3b7e62ffd8d9bed6a5d7f1e47
Xavier Barnada <xbarnada@gisce.net>
2016-05-11 13:02:01 +0200

    Add type to exception

--- libcnmc/res_4131/POS.py
+++ libcnmc/res_4131/POS.py
@@ -128,7 +128,7 @@ class POS(MultiprocessBased):
                 ]
 
                 self.output_q.put(output)
-            except:
+            except Exception:
                 traceback.print_exc()
                 if self.raven:
                     self.raven.captureException()
