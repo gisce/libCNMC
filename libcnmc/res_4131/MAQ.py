d554632240b4a76a8ba6c74b78bc7030946c6ee9
Xavier Barnada <xbarnada@gisce.net>
2016-05-11 13:27:44 +0200

    Add exception type

--- libcnmc/res_4131/MAQ.py
+++ libcnmc/res_4131/MAQ.py
@@ -167,7 +167,7 @@ class MAQ(MultiprocessBased):
                 ]
 
                 self.output_q.put(output)
-            except:
+            except Exception:
                 traceback.print_exc()
                 if self.raven:
                     self.raven.captureException()
