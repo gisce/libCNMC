1553d798f3d72e2f1a29f0cea0617098a52dd906
Xavier Barnada <xbarnada@gisce.net>
2016-05-19 09:30:04 +0200

    Fix typo error

--- libcnmc/res_4131/SUB.py
+++ libcnmc/res_4131/SUB.py
@@ -60,7 +60,7 @@ class SUB(MultiprocessBased):
         O = self.connection
         fields_to_read = [
             'name', 'data_industria', 'data_pm', 'id_municipi', 'cini',
-            'descripcio', 'perc_financament', 'data_baixa', 'posicions'
+            'descripcio', 'perc_financament', 'data_baixa', 'posicions',
             'cnmc_tipo_instalacion'
         ]
         data_pm_limit = '{}-01-01'.format(self.year + 1)
@@ -113,7 +113,7 @@ class SUB(MultiprocessBased):
                     estado = '2'
                 else:
                     estado = '0'
-                if 'posicons' in sub:
+                if 'posicions' in sub:
                     num_pos = len(sub['posicions'])
                 else:
                     num_pos = 0
