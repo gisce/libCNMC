1250a7efd31a7c92ee64fd0af4d969bea1f75994
Xavier Barnada <xbarnada@gisce.net>
2016-05-23 12:38:59 +0200

    Add modules for 4771 resolution data representation

---
+++ libcnmc/models/f4_4771.py
@@ -0,0 +1,36 @@
+from cnmcmodel import CNMCModel
+
+
+class F4Res4771(CNMCModel):
+    """
+        Class for forth file of resolution 4771(Posiciones)
+    """
+    fields = [
+        'identificador',
+        'cini',
+        'denominacion',
+        'codigo_tipo_posicion',
+        'codigo_ccaa',
+        'nivel_tension',
+        'participacion',
+        'fecha_aps',
+    ]
+
+    @property
+    def ref(self):
+        return self.store['identificador']
+
+    def __cmp__(self, other):
+        comp_fields = [
+            'cini', 'nivel_tension', 'participacion',
+            'denominacion', 'fecha_aps'
+        ]
+        if self.diff(other, comp_fields):
+            return True
+        else:
+            return False
+
+
+
+
+
---
+++ libcnmc/models/f6_4771.py
@@ -0,0 +1,32 @@
+from cnmcmodel import CNMCModel
+
+
+class F6Res4771(CNMCModel):
+    """
+        Class for sixth file of resolution 4771(Despatxos)
+    """
+
+    fields = [
+        'identificador',
+        'cini',
+        'denominacion',
+        'anio_aps',
+        'valor_inversion'
+    ]
+
+    @property
+    def ref(self):
+        return self.store['identificador'][1:]
+
+    def __cmp__(self, other):
+        # TODO: Add comparsion fields
+        comp_fields = []
+        if self.diff(other, comp_fields):
+            return True
+        else:
+            return False
+
+
+
+
+
