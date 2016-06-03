e525baf008ae4866d367ca49bf1e3b69244539d9
Xavier Barnada <xbarnada@gisce.net>
2016-06-03 14:16:28 +0200

    Fix schema definition

--- libcnmc/models/f4_4771.py
+++ libcnmc/models/f4_4771.py
@@ -9,14 +9,14 @@ class F4Res4771(CNMCModel):
         Class for forth file of resolution 4771(Posiciones)
     """
     schema = OrderedDict([
-        {'identificador': String()},
-        {'cini': String()},
-        {'denominacion': String()},
-        {'codigo_tipo_posicion': String()},
-        {'codigo_ccaa': Integer()},
-        {'nivel_tension': Decimal(2)},
-        {'participacion': Decimal(2)},
-        {'fecha_aps': String()},
+        ('identificador', String()),
+        ('cini', String()),
+        ('denominacion', String()),
+        ('codigo_tipo_posicion', String()),
+        ('codigo_ccaa', Integer()),
+        ('nivel_tension', Decimal(2)),
+        ('participacion', Decimal(2)),
+        ('fecha_aps', String()),
     ])
 
     @property
