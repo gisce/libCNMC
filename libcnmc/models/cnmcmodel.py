27b89c0d13d03815259f635c142827186e61dba2
Xevi <xbarnada@gmail.com>
2016-06-15 21:03:55 +0200

    Remove unneded code

--- libcnmc/models/cnmcmodel.py
+++ libcnmc/models/cnmcmodel.py
@@ -53,8 +53,6 @@ class CNMCModel(object):
         self.validator = CNMCValidator(self.schema)
         stored = namedtuple('{0}_store'.format(self.__class__.__name__), self.fields)
         self.store = stored(*evalues, **kwvalues)
-        for key in self.store.keys():
-            self.store[key] = self.store[key].encode('utf-8')
         self.validator.validate(self.store._asdict())
         self.store = stored(**self.validator.document)
 
