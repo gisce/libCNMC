aa46c3682d90225c369d5d34daf63fe06cef92fe
AdriNicolau <anicolau@gisce.net>
2017-07-05 13:32:17 +0200

    Add support to the new field Tram_ID on celles.

--- libcnmc/cir_4_2015/F15.py
+++ libcnmc/cir_4_2015/F15.py
@@ -84,6 +84,35 @@ class F15(MultiprocessBased):
                     tram_name = "A{0}".format(tram_name)
         return node, vertex, tram_name
 
+    def get_node_vertex(self, element_name):
+        o = self.connection
+        node = ''
+        vertex = None
+        bloc = None
+        if element_name:
+            # Search on the diferent models
+            models = [o.GiscegisBlocsInterruptorat,
+                      o.GiscegisBlocsFusiblesat,
+                      o.GiscegisBlocsSeccionadorat,
+                      o.GiscegisBlocsSeccionadorunifilar]
+            for model in models:
+                bloc_id = model.search([('codi', '=', element_name)])
+                if bloc_id:
+                    model_ok = model
+                    break
+            if bloc_id:
+                bloc = model_ok.read(
+                    bloc_id[0], ['node', 'vertex'])
+                v = o.GiscegisVertex.read(bloc['vertex'][0], ['x', 'y'])
+                if bloc.get('node', False):
+                    node = bloc['node'][1]
+                else:
+                    node = v['id']
+                if bloc.get('vertex', False):
+                    vertex = (round(v['x'], 3), round(v['y'], 3))
+
+        return node, vertex
+
     def obtenir_camps_linia(self, installacio):
         o = self.connection
         valor = installacio.split(',')
@@ -114,7 +143,7 @@ class F15(MultiprocessBased):
     def consumer(self):
         o = self.connection
         fields_to_read = [
-            'installacio', 'cini', 'propietari', 'name'
+            'installacio', 'cini', 'propietari', 'name', 'tram_id'
         ]
         while True:
             try:
@@ -127,9 +156,13 @@ class F15(MultiprocessBased):
                 dict_linia = self.obtenir_camps_linia(celles['installacio'])
                 o_fiabilitat = celles['name']
 
-                o_node, vertex, o_tram = self.get_node_vertex_tram(o_fiabilitat)
-                if celles["ccamp"]:
-                    o_tram = camp
+                if not celles['tram_id']:
+                    o_node, vertex, o_tram = self.get_node_vertex_tram(
+                        o_fiabilitat)
+                else:
+                    o_tram = celles['tram_id']
+                    o_node, vertex = self.get_node_vertex(o_fiabilitat)
+
                 o_node = o_node.replace('*', '')
                 o_cini = celles['cini']
                 z = ''
