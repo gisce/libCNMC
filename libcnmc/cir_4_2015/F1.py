9cfcfc6c8f6f7457fcbc73b27640823cc68feb45
xbarnada <xbarnada@gisce.net>
2019-05-27 16:10:03 +0200

    Use node name

--- libcnmc/cir_4_2015/F1.py
+++ libcnmc/cir_4_2015/F1.py
@@ -285,7 +285,7 @@ class F1(MultiprocessBased):
                 if cups['id_municipi']:
                     id_mun = cups["id_municipi"][0]
                     o_codi_ine_prov, o_codi_ine_mun = self.get_ine(id_mun)
-                o_nom_node = cups.get("node_id", "")
+                o_nom_node = cups.get("node_id")[1]
                 o_tensio = ''
                 o_connexio = ''
                 vertex = False
