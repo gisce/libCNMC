fa76b95a970e1abd3b5eb09bd08d59cb65c40fd0
anicolau <anicolau@gisce.net>
2019-06-06 15:19:00 +0200

    Add new option to the 4/2015 F1.

--- libcnmc/cli_4_2015.py
+++ libcnmc/cli_4_2015.py
@@ -76,7 +76,8 @@ def cir_4_2015_f10at(**kwargs):
 @click.option('-d', '--database', help='Nom de la base de dades')
 @click.option('--num-proc', default=N_PROC, type=click.INT)
 @click.option('--derechos/--no-derechos', default=False)
-@click.option("--reducir-cups/--no-reducir-cups",default=False)
+@click.option("--reducir-cups/--no-reducir-cups", default=False)
+@click.option("--allow-cna/--no-allow-cna", default=False)
 def cir_4_2015_f1(**kwargs):
     """
     Click entry to generate the F1 of 4/2015
@@ -98,7 +99,8 @@ def cir_4_2015_f1(**kwargs):
         codi_r1=kwargs['codi_r1'],
         year=kwargs['year'],
         derechos=kwargs["derechos"],
-        reducir_cups=kwargs["reducir_cups"]
+        reducir_cups=kwargs["reducir_cups"],
+        allow_cna=kwargs["allow_cna"]
     )
     proc.calc()
 
