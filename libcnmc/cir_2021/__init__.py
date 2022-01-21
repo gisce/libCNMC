f5359fdaf6026ff8725e24126a181002f3d93f75
scampos <scampos@gisce.net>
2022-01-21 10:58:21 +0100

    Rename and B1_1

--- libcnmc/cir_2021/__init__.py
+++
@@ -1,2 +0,0 @@
-from FA1 import FA1
-from FA4 import FA4
--- libcnmc/cli_8_2021.py
+++ libcnmc/cli_8_2021.py
@@ -2,15 +2,15 @@
 import click
 from libcnmc.utils import N_PROC
 from libcnmc.core.backend import OOOPFactory
-from libcnmc import cir_2021
+from libcnmc import cir_8_2021
 from datetime import datetime
 
 
 @click.group()
-def cnmc_2021():
+def cnmc_8_2021():
     pass
 
-@cnmc_2021.command()
+@cnmc_8_2021.command()
 @click.option('-q', '--quiet', default=False,
               help="No mostrar missatges de status per stderr")
 @click.option('--interactive/--no-interactive', default=True,
@@ -44,7 +44,7 @@ def cir_2021_fa1(**kwargs):
     O = OOOPFactory(dbname=kwargs['database'], user=kwargs['user'],
                     pwd=kwargs['password'], port=kwargs['port'],
                     uri=kwargs['server'])
-    proc = cir_2021.FA1(
+    proc = cir_8_2021.FA1(
         quiet=kwargs['quiet'],
         interactive=kwargs['interactive'],
         output=kwargs['output'],
@@ -60,7 +60,7 @@ def cir_2021_fa1(**kwargs):
     proc.calc()
 
 
-@cnmc_2021.command()
+@cnmc_8_2021.command()
 @click.option('-q', '--quiet', default=False,
               help="No mostrar missatges de status per stderr")
 @click.option('--interactive/--no-interactive', default=True,
@@ -80,7 +80,7 @@ def cir_2021_fa1(**kwargs):
 @click.option('--num-proc', default=N_PROC, type=click.INT)
 @click.option('--derechos/--no-derechos', default=False)
 @click.option("--reducir-cups/--no-reducir-cups",default=False)
-def cir_2021_fa4(**kwargs):
+def cir_8_2021_fa4(**kwargs):
     """
     Click entry to generate the FA4 file of 2021
 
@@ -92,7 +92,52 @@ def cir_2021_fa4(**kwargs):
     O = OOOPFactory(dbname=kwargs['database'], user=kwargs['user'],
                     pwd=kwargs['password'], port=kwargs['port'],
                     uri=kwargs['server'])
-    proc = cir_2021.FA4(
+    proc = cir_8_2021.FA4(
+        quiet=kwargs['quiet'],
+        interactive=kwargs['interactive'],
+        output=kwargs['output'],
+        connection=O,
+        num_proc=kwargs['num_proc'],
+        codi_r1=kwargs['codi_r1'],
+        year=kwargs['year'],
+        derechos=kwargs['derechos'],
+        reducir_cupss=kwargs["reducir_cups"]
+    )
+    proc.calc()
+
+@cnmc_8_2021.command()
+@click.option('-q', '--quiet', default=False,
+              help="No mostrar missatges de status per stderr")
+@click.option('--interactive/--no-interactive', default=True,
+              help="Deshabilitar el mode interactiu")
+@click.option('-o', '--output', help="Fitxer de sortida")
+@click.option('-c', '--codi-r1', help='Codi R1 de la distribuidora')
+@click.option('-y', '--year', default=(datetime.now().year - 1),
+              help=u"Any per càlculs")
+@click.option('-s', '--server', default='http://localhost',
+              help=u'Adreça servidor ERP')
+@click.option('-p', '--port', default=8069,
+              help='Port servidor ERP', type=click.INT)
+@click.option('-u', '--user', default='admin', help='Usuari servidor ERP')
+@click.option('-w', '--password', default='admin',
+              help='Contrasenya usuari ERP')
+@click.option('-d', '--database', help='Nom de la base de dades')
+@click.option('--num-proc', default=N_PROC, type=click.INT)
+@click.option('--derechos/--no-derechos', default=False)
+@click.option("--reducir-cups/--no-reducir-cups",default=False)
+def cir_2021_fb1_1(**kwargs):
+    """
+    Click entry to generate the FB1.1 file of 2021
+
+    :param kwargs: Params to pass to the process
+    :type kwargs:dict(str,str)
+    :return: None
+    :rtype: None
+    """
+    O = OOOPFactory(dbname=kwargs['database'], user=kwargs['user'],
+                    pwd=kwargs['password'], port=kwargs['port'],
+                    uri=kwargs['server'])
+    proc = cir_2021.FB1_1(
         quiet=kwargs['quiet'],
         interactive=kwargs['interactive'],
         output=kwargs['output'],
