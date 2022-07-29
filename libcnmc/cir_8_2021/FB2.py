28026919e5aac2865365c290db09434b04cc8bcb
Bartro <mbartroli@gisce.net>
2022-07-29 12:54:43 +0200

    fb2

--- libcnmc/cir_8_2021/FB2.py
+++ libcnmc/cir_8_2021/FB2.py
@@ -139,8 +139,8 @@ class FB2(MultiprocessBased):
 
                 ct = O.GiscedataCts.read(item, fields_to_read)
 
-                linia = O.GiscedataProjecteObraTiCts.read([item], fields_to_read_obra)[0]
-
+                linia = O.GiscedataProjecteObraTiCts.read(item, fields_to_read_obra)
+                
                 data_ip = convert_spanish_date(
                     linia['fecha_aps'] if not linia['fecha_baja']
                                           and linia['tipo_inversion'] != '1' else ''
