5c17fafdbeac80a0c20e5e6929896bf67c5c8fc7
Bartro <mbartroli@gisce.net>
2022-09-01 09:37:34 +0200

    FB4

--- libcnmc/cir_8_2021/FB4.py
+++ libcnmc/cir_8_2021/FB4.py
@@ -200,7 +200,7 @@ class FB4(MultiprocessBased):
                     im_obracivil = format_f_6181(linia['im_obracivil'] or 0.0, float_type='euro')
                     im_construccion = str(format_f(
                         float(im_materiales.replace(",", ".")) + float(im_obracivil.replace(",", "."))
-                    ), 2).replace(".", ",")
+                    , 2)).replace(".", ",")
                     im_ingenieria = format_f_6181(linia['im_ingenieria'] or 0.0, float_type='euro')
                     im_trabajos = format_f_6181(linia['im_trabajos'] or 0.0, float_type='euro')
                     tipo_inversion = (linia['tipo_inversion'] or '0') if not linia['fecha_baja'] else '1'
