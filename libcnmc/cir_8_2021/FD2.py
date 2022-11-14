e97a7c91a9b6122e1d911f1e259c89ad5925573d
ruben <rubenolesti@gmail.com>
2022-11-14 12:56:49 +0100

    FD2 R1s

--- libcnmc/cir_8_2021/FD2.py
+++ libcnmc/cir_8_2021/FD2.py
@@ -69,8 +69,8 @@ class FD2(MultiprocessBased):
                 item = self.input_q.get()
                 self.progress_q.put(item)
 
-                file_fields = {'totals': 0}
-                z8_fields = {'totals': 0}
+                file_fields = {'totals': 0, 'dentro_plazo': 0, 'fuera_plazo': 0, 'no_tramitadas': 0 }
+                z8_fields = {'totals': 0, 'dentro_plazo': 0, 'fuera_plazo': 0, 'no_tramitadas': 0 }
 
                 year_start = '01-01-' + str(self.year)
                 year_end = '12-31-' + str(self.year)
